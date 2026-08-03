11833cbb061444b605207262a61b33c69c126afa
ymeister <47071325+ymeister@users.noreply.github.com>
2026-08-03 22:36:03 +0900

    Update `haskell-nix` pin
    0093d8e2098d42c9f7ed76e959cb6d3b39feaeb8. Its fresher Hackage index
    (with the example's `allow-newer: *:*`) solves to some 1.1 and
    semialign 1.4, which prim-uniq 0.2 and reflex 0.9.4.1 predate; the
    example constrains both to their supported majors.

--- examples/reflex-todomvc/project.nix
+++ examples/reflex-todomvc/project.nix
@@ -9,6 +9,26 @@
   src = ./.;
   compiler-nix-name = "ghc914";
 
+  source-repository-packages = {
+    reflex-dom = {
+      src = ./deps/reflex-dom;
+      subdir = [ "reflex-dom" "reflex-dom-core" ];
+    };
+  };
+
+  haskell-nix = {
+    extraSrcFiles = {
+      library.extraSrcFiles = [
+        "static/style.css"
+      ];
+      exes.reflex-todomvc.extraSrcFiles = [
+        "static/style.css"
+      ];
+    };
+
+    options.shell.withHaddock = false;
+  };
+
   nixpkgs = {
     # webkitgtk (via jsaddle-webkit2gtk) still links libsoup 2
     pkgs = import config.inputs.nixpkgs {
@@ -21,24 +41,6 @@
       (self: super: { chrome-test-utils = null; })
     ];
   };
-
-  source-repository-packages = {
-    reflex-dom = {
-      src = ./deps/reflex-dom;
-      subdir = [ "reflex-dom" "reflex-dom-core" ];
-    };
-  };
-
-  haskell-nix.extraSrcFiles = {
-    library.extraSrcFiles = [
-      "static/style.css"
-    ];
-    exes.reflex-todomvc.extraSrcFiles = [
-      "static/style.css"
-    ];
-  };
-
-  haskell-nix.options.shell.withHaddock = false;
 
   shell = {
     crossPlatforms = ps: with ps; [ ghcjs wasi32 ];
