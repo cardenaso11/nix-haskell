e10422a59e1c6a6ce71eb940b6b56ea0c25f3869
ymeister <47071325+ymeister@users.noreply.github.com>
2026-08-06 19:43:44 +0900

    Expose the wasm-meta variants' shells in reflex-todomvc

--- examples/reflex-todomvc/shell.nix
+++ examples/reflex-todomvc/shell.nix
@@ -5,4 +5,7 @@ let project = import ./default.nix { inherit system inputs; };
 in {
   haskell-nix = project.haskell-nix.shell;
   nixpkgs = project.nixpkgs.shell;
+
+  haskell-nix-wasm-meta = project.haskell-nix-wasm-meta.shell;
+  nixpkgs-wasm-meta = project.nixpkgs-wasm-meta.shell;
 }
